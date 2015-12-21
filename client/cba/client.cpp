ca4d3b6df9e2f5e1f4082a3ea6b47f75edc5db0e
hlebloika <hleb_loika@epam.com>
2015-12-21 04:27:37 +0300

    Add server handler on shutdown, modify client to start listen port in case of disconnect

--- client/cba/client.cpp
+++ client/cba/client.cpp
@@ -2,24 +2,71 @@
 #include <WinSock2.h>
 #include <iostream>
 #include <WS2tcpip.h>
+#include <vector>
 
 
-SOCKET Client;
+SOCKET Client, Server;
 STARTUPINFO sa;
 PROCESS_INFORMATION pi;
+HANDLE th;
+char acc_buf[256];
+WSAEVENT NewEvent, NewEvent2, pEvents[10];
+struct addrinfo hints, *result;
+
+DWORD WINAPI UpServer()
+{
+	struct addrinfo hints_ser;
+	struct addrinfo * result_ser;
+
+	ZeroMemory(&hints_ser, sizeof(hints_ser));
+
+	hints_ser.ai_family = AF_INET;
+	hints_ser.ai_flags = AI_PASSIVE;
+	hints_ser.ai_socktype = SOCK_STREAM;
+	hints_ser.ai_protocol = IPPROTO_TCP;
+
+	getaddrinfo(NULL, "10060", &hints_ser, &result_ser);
+
+	Server = socket(result_ser->ai_family, result_ser->ai_socktype, result_ser->ai_protocol);
+	if (Server == INVALID_SOCKET)
+	{
+		printf("Error create socket\n");
+	}
+	int bi = bind(Server, result_ser->ai_addr, result_ser->ai_addrlen);
+	if (bi == SOCKET_ERROR)
+	{
+		printf("Error bind socket\n");
+	}
+	int li = listen(Server, SOMAXCONN);
+	if (li == SOCKET_ERROR)
+	{
+		printf("Error listen socket\n");
+	}
+	closesocket(Client);
+	Client = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	int co = connect(Client, result->ai_addr, int(result->ai_addrlen));
+	if (co == SOCKET_ERROR)
+	{
+		printf("Error connecting socket: %d\n", WSAGetLastError());
+	}
+
+	return 0;
+}
 
 int main()
 {
 	WSADATA wsaData;
+	FD_SET WriteSet;
+	FD_SET ReadSet;
 	int res;
 	int sw;
 	char send_buf[1024] = "Hi";
-	char acc_buf[1024];
+	char m_connect[] = "Connect...";
 	size_t size;
 	int oi;
 
 
-	struct addrinfo hints, *result;
+	
 	ZeroMemory(&sa, sizeof(sa));
 	ZeroMemory(&pi, sizeof(pi));
 	ZeroMemory(&hints, sizeof(hints));
@@ -30,46 +77,59 @@ int main()
 
 	res = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	getaddrinfo(NULL, "10061", &hints, &result);
+	getaddrinfo(NULL, "10060", &hints, &result);
 
 	std::cout << "#####################" << std::endl;
 	std::cout << "###Welcome to Chat###" << std::endl;
 	std::cout << "#####################" << std::endl;
 
-	std::cout << "Select the options: " << std::endl;
-	std::cout << "1) Start Server" << std::endl;
-	std::cout << "2) Connect as a client" << std::endl;
-	std::cout << "Your option: ";
-	std::cin >> sw;
-	switch (sw)
-	{
-	case 1:
+	NewEvent = WSACreateEvent();
+	NewEvent2 = WSACreateEvent();
+	WSANETWORKEVENTS hProcessEvent;
+	WSANETWORKEVENTS hProcessEvent2;
+	Client = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	connect(Client, result->ai_addr, int(result->ai_addrlen));
+	WSAEventSelect(Client, NewEvent, FD_READ | FD_WRITE );
+	WSAEventSelect(Server, NewEvent2, FD_CONNECT);
+	pEvents[0] = NewEvent;
+	pEvents[1] = NewEvent2;
 
-		if (!CreateProcess(TEXT("D:\\Kyrsach\\loiko\\client\\Debug\\server.exe"), NULL, NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &sa, &pi));
+
+
+
+	while (1)
+	{
+		WSAWaitForMultipleEvents(2, pEvents, FALSE, INFINITE, FALSE);
+		WSAEnumNetworkEvents(Client, NewEvent, &hProcessEvent);
+		WSAEnumNetworkEvents(Server, NewEvent2, &hProcessEvent2);
+
+		if (hProcessEvent.lNetworkEvents & FD_READ)
 		{
-			printf("Failed (%d)", GetLastError());
+			memset(acc_buf, 0, sizeof(acc_buf));
+			int bytes = recv(Client, acc_buf, 1024, FALSE);
+			printf(acc_buf);
 		}
 
-	case 2:
-		Client = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-		connect(Client, result->ai_addr, int(result->ai_addrlen));
-		for (;; Sleep(10))
+		if (hProcessEvent2.lNetworkEvents & FD_CONNECT)
 		{
-			memset(&acc_buf, 0, strlen(acc_buf));
-			memset(&send_buf, 0, strlen(send_buf));
-			recv(Client, acc_buf, 1024, NULL);
-			size = strlen(acc_buf);
-			for (int i = 0; i < size; i++)
-			{
-				if (acc_buf[i] == '$')
-					printf("\n");
-				else
-					printf("%c", acc_buf[i]);
-			}
-			printf("\n");
-			std::cout << "Enter the message: ";
+			accept(Client, NULL, NULL);
+			send(Client, m_connect, strlen(m_connect), NULL);
+		}
+		else
+		{
+			printf("Enter the message: ");
 			gets(send_buf);
-			send(Client, send_buf, sizeof(send_buf), NULL);
+			res = send(Client, send_buf, 1024, NULL);
+			if (res == SOCKET_ERROR && WSAGetLastError() == 10054)
+			{
+				int bytes = recv(Client, acc_buf, 1024, FALSE);
+				if (bytes)
+				{
+					HANDLE hl = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)UpServer, NULL, 0, NULL);
+					WaitForSingleObject(hl, INFINITE);
+				}
+			}
+
 		}
 	}
 }
--- server/abc/server.cpp
+++ server/abc/server.cpp
@@ -1,15 +1,40 @@
 #pragma comment(lib, "Ws2_32.lib")
 #include <WinSock2.h>
 #include <iostream>
+#include <vector>
 #include <WS2tcpip.h>
 
 SOCKET Connect;
-SOCKET* Connections;
+struct client_data
+{
+	SOCKET sock;
+	struct addrinfo inf;
+} cl_d;
+
+SOCKET Connections[10];
+
 SOCKET Listen;
-char endline = '$';
+char endline = '\0';
 int ClientCount = 0;
 std::string str;
+char * m_stop = "up";
 
+BOOL CtrlHandler(DWORD fdwCtrlType)
+{
+	switch (fdwCtrlType)
+	{
+	case CTRL_C_EVENT:
+		send(Connections[0], m_stop, strlen(m_stop), NULL);
+		printf("Send before die...\n");
+		closesocket(Listen);
+		exit(0);
+	case CTRL_CLOSE_EVENT:
+		send(Connections[0], m_stop, strlen(m_stop), NULL);
+		printf("Send before close...\n");
+		system("pause");
+		exit(0);
+	}
+}
 void SendMessageToClient(int ID)
 {
 	char* buffer = new char[1024];
@@ -17,15 +42,17 @@ void SendMessageToClient(int ID)
 	{
 		memset(buffer, 0, sizeof(buffer));
 		if (recv(Connections[ID], buffer, 1024, NULL))
-		{
-			size_t size = strlen(buffer);
-			buffer[size] = endline;
-			printf(buffer);
-			printf("\n");
-			for (int i = 0; i <= ClientCount; i++)
-			{
-				send(Connections[i], buffer, strlen(buffer), NULL);
-			}
+		{			
+				size_t size = strlen(buffer);
+				buffer[size] = '\n';
+				buffer[size + 1] = endline;
+				printf(buffer);
+				printf("\n");
+				for (int i = 0; i <= ClientCount; i++)
+				{
+					send(Connections[i], buffer, strlen(buffer), NULL);
+				}
+			
 		}
 	}
 	delete buffer;
@@ -45,7 +72,7 @@ int main()
 	struct addrinfo hints;
 	struct addrinfo * result;
 
-	Connections = (SOCKET*)calloc(64, sizeof(SOCKET));
+	//Connections = (SOCKET*)calloc(64, sizeof(SOCKET));
 	ZeroMemory(&hints, sizeof(hints));
 
 	hints.ai_family = AF_INET;
@@ -53,7 +80,7 @@ int main()
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
 
-	getaddrinfo(NULL, "10061", &hints, &result);
+	getaddrinfo(NULL, "10060", &hints, &result);
 
 	Listen = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 	bind(Listen, result->ai_addr, result->ai_addrlen);
@@ -65,6 +92,7 @@ int main()
 	char m_connect[] = "Connect...";
 	for (;; Sleep(75))
 	{
+		SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
 		if (Connect = accept(Listen, NULL, NULL))
 		{
 			printf("Client connect...\n");
@@ -72,8 +100,6 @@ int main()
 			send(Connections[ClientCount], m_connect, strlen(m_connect), NULL);
 			ClientCount++;
 			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)SendMessageToClient, (LPVOID)(ClientCount - 1), NULL, NULL);
-	//		WaitForSingleObject(tr, INFINITE);
-	//		printf("End thread");
 		}
 	}
 	return 0;
